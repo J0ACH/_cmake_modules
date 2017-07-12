e9a79561a74dc96d5c0fa46f8de1ad26366c0068
J0ACH <jachym.pesek@gmail.com>
2017-07-12 17:25:51 +0200

    gitModule fixed

--- GitModule.cmake
+++ GitModule.cmake
@@ -89,8 +89,7 @@ function (GitClone)
 	message(STATUS "GitInitBuildFile: " ${_GitDownloadDir})
 
 	file(MAKE_DIRECTORY ${GitClone_PATH})
-	setDier
-	
+		
 	set(BuildFile 
 		"cmake_minimum_required(VERSION 2.8.2)\n"
 		"project(${GitClone_NAME}-download NONE)\n"
@@ -99,18 +98,10 @@ function (GitClone)
 		"ExternalProject_Add(${GitClone_NAME}-download"
 			"\tGIT_REPOSITORY      ${GitClone_GIT}"
 			"\tGIT_TAG			   master"
-			#"\tPREFIX		       ${_GitDownloadDir}"
 			"\tDOWNLOAD_DIR		   ${_GitCacheDir}" 
 			"\tSOURCE_DIR          ${_GitSourceDir}"
-			#"\tSOURCE_DIR          ${_GitMainDir}"
 			"\tBINARY_DIR          ${_GitCacheDir}"
-			"\tINSTALL_DIR          ${_GitCacheDir}"
-			#"\tCONFIGURE_COMMAND   \"\""
-			#"\tBUILD_COMMAND       \"\""
-			#"\tINSTALL_COMMAND     \"\""
-			#"\tTEST_COMMAND        \"\""
-			#"\tCMAKE_ARGS		   -DCMAKE_BINARY_DIR=${_GitCacheDir}"
-			#"-DCMAKE_SOURCE_DIR=${_GitMainDir}"
+			"\tCMAKE_ARGS		   -DCMAKE_INSTALL_PREFIX=${_GitMainDir}"
 		")"
 	)
 
@@ -123,7 +114,6 @@ function (GitClone)
 	
 	file(WRITE ${_GitDownloadDir}/CMakeLists.txt ${ConfigTxt})
 
-	#[[
     execute_process(
 		COMMAND ${CMAKE_COMMAND} -G ${CMAKE_GENERATOR}
 		RESULT_VARIABLE result
@@ -141,33 +131,6 @@ function (GitClone)
 	if(result)
         message(FATAL_ERROR "Build step for ${GitClone_NAME} failed: ${result}")
     endif()
-	#]]
-	#[[
 	
-		INCLUDE(ExternalProject)
-		ExternalProject_Add ( SharedLib
-			GIT_REPOSITORY https://github.com/J0ACH/_template_SharedLib.git
-			GIT_TAG "master"
-			SOURCE_DIR "${CMAKE_BINARY_DIR}/SharedLib-src"
-			BINARY_DIR "${CMAKE_BINARY_DIR}/SharedLib-build"
-			UPDATE_COMMAND ""
-			CMAKE_GENERATOR "Visual Studio 15 2017 Win64"
-	
-			#DOWNLOAD_DIR
-			#INSTALL_DIR ${BUILD_DIR}
-			#INSTALL_COMMAND ${SharedLib_inst_comm} 
-			#INSTALL_DIR ${BUILD_DIR}/install
-  			CMAKE_ARGS
-			"-DCMAKE_CONFIGURATION_TYPES=${CMAKE_CONFIGURATION_TYPES}"
-			"-DBUILD_DIR=${EXTERNAL_BUILD_LOCATION}"
-			"-DCMAKE_INSTALL_PREFIX=${BUILD_DIR}"
-		)
-		configure_file(CMakeLists.txt.in googletest-download/CMakeLists.txt)
-		execute_process(COMMAND "${CMAKE_COMMAND}" -G "${CMAKE_GENERATOR}" .
-		WORKING_DIRECTORY "${CMAKE_BINARY_DIR}/googletest-download" )
-		execute_process(COMMAND "${CMAKE_COMMAND}" --build .
-		WORKING_DIRECTORY "${CMAKE_BINARY_DIR}/googletest-download" )
-	#]]
-
 	message(STATUS "GitClone macro done...\n")
 endfunction (GitClone)
